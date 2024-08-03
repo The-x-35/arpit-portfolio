d1ab075c528ab054dc0d46e2e10078d3a41186a4
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-08-04 02:53:49 +0530

    Added pc for desktop devices

--- src/app/components/First.tsx
+++ src/app/components/First.tsx
@@ -1,26 +1,32 @@
 import { motion } from 'framer-motion';
 import React from 'react';
+import ComputersCanvas from './ComputerCanvas';
 
 const First: React.FC = () => {
   return (
      <section className={`w-full h-screen mx-auto`}>
-      <div
-        className={"absolute inset-0 top-[120px]  max-w-7xl mx-auto sm:px-16 px-6 flex flex-row items-start gap-5"}
-      >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
-        </div>
+        <div className='flex flex-col justify-center items-center'>
+          <div
+            className={"absolute inset-0 top-[120px] left-10 max-w-7xl mx-auto px-6 flex flex-row items-start gap-5"}
+          >
+            <div className='flex flex-col justify-center items-center mt-5'>
+              <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+              <div className='w-1 sm:h-80 h-40 violet-gradient' />
+            </div>
 
-        <div>
-          <h1 className={"font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 text-white"}>
-            Hi, I'm <span className='text-[#915EFF]'>Arpit</span>
-          </h1>
-          <p className={"text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-white-100"}>
-            I am a Cyber Security Enthusiast, <br className='sm:block hidden' />
-            Web and Software Developer.
-          </p>
-         
+            <div>
+              <h1 className={"font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 text-white"}>
+                Hi, I'm <span className='text-[#915EFF]'>Arpit</span>
+              </h1>
+              <p className={"text-[#dfd9ff] font-medium lg:text-[21px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-white-100"}>
+                I am a Cyber Security Enthusiast, <br className='sm:block hidden' />
+                Web and Software Developer.
+              </p>
+            
+            </div>
+              <div className='flex flex-col justify-center items-center -mt-48 mx-auto -mr-10 h-full w-full -mb-96 -bottom-96'>
+              <ComputersCanvas />
+              </div>
         </div>
         <div className='absolute xs:bottom-10 bottom-5 right-1 w-full flex justify-center items-center'>
         <a href='#about'>
