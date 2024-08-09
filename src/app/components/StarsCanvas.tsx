599bd27b61857fc0ea4a267bb3225699c4d4658a
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-08-09 11:48:05 +0530

    Prevented rocket drag accidently by mouse, put gif instead of stars canvas to improve performance

--- src/app/components/StarsCanvas.tsx
+++ src/app/components/StarsCanvas.tsx
@@ -1,92 +1,95 @@
 'use client';
+import "./StarsCanvas.css" ;
+// import React, { useRef, useState, Suspense } from 'react';
+// import { Canvas, useFrame } from '@react-three/fiber';
+// import * as THREE from 'three';
+// import { Preload } from '@react-three/drei';
 
-import React, { useRef, useState, Suspense } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
-import { Preload } from '@react-three/drei';
+// const createCircleTexture = () => {
+//   const size = 128;
+//   const canvas = document.createElement('canvas');
+//   canvas.width = size;
+//   canvas.height = size;
+//   const context = canvas.getContext('2d');
+//   if (!context) {
+//     throw new Error('Failed to get canvas context');
+//   }
 
-const createCircleTexture = () => {
-  const size = 128;
-  const canvas = document.createElement('canvas');
-  canvas.width = size;
-  canvas.height = size;
-  const context = canvas.getContext('2d');
-  if (!context) {
-    throw new Error('Failed to get canvas context');
-  }
+//   // Draw a white circle on a black background
+//   context.fillStyle = 'black';
+//   context.fillRect(0, 0, size, size);
+//   context.beginPath();
+//   context.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
+//   context.fillStyle = 'white';
+//   context.fill();
 
-  // Draw a white circle on a black background
-  context.fillStyle = 'black';
-  context.fillRect(0, 0, size, size);
-  context.beginPath();
-  context.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
-  context.fillStyle = 'white';
-  context.fill();
+//   // Apply some anti-aliasing
+//   context.globalCompositeOperation = 'destination-in';
+//   context.beginPath();
+//   context.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
+//   context.fill();
 
-  // Apply some anti-aliasing
-  context.globalCompositeOperation = 'destination-in';
-  context.beginPath();
-  context.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
-  context.fill();
+//   return new THREE.CanvasTexture(canvas);
+// };
 
-  return new THREE.CanvasTexture(canvas);
-};
+// const Stars: React.FC = () => {
+//   const ref = useRef<THREE.Points>(null);
+//   const [positions] = useState(() => {
+//     const positions = new Float32Array(5000 * 3);
+//     for (let i = 0; i < 5000; i++) {
+//       const x = (Math.random() - 0.5) * 2;
+//       const y = (Math.random() - 0.5) * 2;
+//       const z = (Math.random() - 0.5) * 2;
+//       positions.set([x, y, z], i * 3);
+//     }
+//     return positions;
+//   });
 
-const Stars: React.FC = () => {
-  const ref = useRef<THREE.Points>(null);
-  const [positions] = useState(() => {
-    const positions = new Float32Array(5000 * 3);
-    for (let i = 0; i < 5000; i++) {
-      const x = (Math.random() - 0.5) * 2;
-      const y = (Math.random() - 0.5) * 2;
-      const z = (Math.random() - 0.5) * 2;
-      positions.set([x, y, z], i * 3);
-    }
-    return positions;
-  });
+//   useFrame((state, delta) => {
+//     if (ref.current) {
+//       ref.current.rotation.x -= delta / 10;
+//       ref.current.rotation.y -= delta / 15;
+//     }
+//   });
 
-  useFrame((state, delta) => {
-    if (ref.current) {
-      ref.current.rotation.x -= delta / 10;
-      ref.current.rotation.y -= delta / 15;
-    }
-  });
+//   // Create a circle texture programmatically
+//   const texture = createCircleTexture();
 
-  // Create a circle texture programmatically
-  const texture = createCircleTexture();
-
-  return (
-    <points ref={ref}>
-      <bufferGeometry>
-        <bufferAttribute
-          attach="attributes-position"
-          array={positions}
-          count={positions.length / 3}
-          itemSize={3}
-        />
-      </bufferGeometry>
-      <pointsMaterial
-        map={texture}
-        size={0.002} // Adjust size as needed
-        sizeAttenuation={true}
-        depthWrite={false}
-        transparent
-        alphaTest={0.5} // Ensure that only the circle part of the texture is rendered
-      />
-    </points>
-  );
-};
+//   return (
+//     <points ref={ref}>
+//       <bufferGeometry>
+//         <bufferAttribute
+//           attach="attributes-position"
+//           array={positions}
+//           count={positions.length / 3}
+//           itemSize={3}
+//         />
+//       </bufferGeometry>
+//       <pointsMaterial
+//         map={texture}
+//         size={0.002} // Adjust size as needed
+//         sizeAttenuation={true}
+//         depthWrite={false}
+//         transparent
+//         alphaTest={0.5} // Ensure that only the circle part of the texture is rendered
+//       />
+//     </points>
+//   );
+// };
 
 const StarsCanvas: React.FC = () => {
   return (
-    <div className="w-full h-screen absolute inset-0 z-[-1]">
-      <Canvas camera={{ position: [0, 0, 1] }}>
+    <>
+    <div className="w-full h-screen absolute inset-0 z-[-1]" style={{ backgroundImage: `url('bg.gif')` }}>
+      {/* <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
         <Preload all />
-      </Canvas>
+      </Canvas> */}
     </div>
+    <div className="absolute inset-0 z-[-1] bg-overlay"></div>
+    </>
   );
 };
 
